b95ab7c914415b6d1c3ecbf133b049c168907c15
YummyLucky <619354210@qq.com>
2018-03-28 16:02:47 +0800

    test complete,already can took the data

--- fdb_test2.py
+++ fdb_test2.py
@@ -1,9 +1,25 @@
-import firebirdsql
-
+#引入firebirdsql库，可用pip安装，pip install firebirdsql
+import firebirdsql as fdb
+# SELECT * FROM MyTable;
 # The server is named 'bison'; the database file is at 'D:\mabo-yummy\GD_access\EKP.fdb'.
-con = firebirdsql.connect(
-    # dsn='bison:/temp/test.fdb',
-    dsn='localhost:D:\mabo-yummy\GD_access\EKP.fdb',
+# 服务器名字，此处本机为‘localhost’
+# 默认user为sysdba，password为masterkey
+con = fdb.connect(
+    # dsn='bison:/temp/test.fdb','D:/mabo-yummy/GD_access/EKP.FDB''D:/testfdb/TEST.fdb'
+    dsn='localhost:D:/mabo-yummy/GD_access/EKP.FDB',
     user='sysdba', password='masterkey',
-    # charset='UTF8' # specify a character set for the connection
+    charset='GBK' # specify a character set for the connection
   )
+# sql = 'SELECT * FROM CONTROL;'
+sql = 'SELECT * FROM HISTALARM;'
+# print(con)
+cur = con.cursor()
+data = cur.execute(sql)
+# print(data.fetchall())
+for i in data:
+    print(i)
+# while True:
+#     row = cur.fetchone()
+#     if not row:
+#         break
+#     print(row)
